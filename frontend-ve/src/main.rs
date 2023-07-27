9a8f8b4afc34e645761ab10759022148f70196f1
Ruk Doe <info@doany.io>
2023-07-28 00:48:17 +0900

    add tailwindcss and router

--- frontend-ve/src/main.rs
+++ frontend-ve/src/main.rs
@@ -1,25 +1,38 @@
+use yew_router::prelude::*;
 use yew::prelude::*;
+mod pages;
+pub mod components;
+use pages::home::Home;
+use pages::search::Search;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/search")]
+    Search,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <Home /> },
+        Route::Search => html! { <Search /> },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
 
 fn main() {
     yew::Renderer::<App>::new().render();
 }
-
